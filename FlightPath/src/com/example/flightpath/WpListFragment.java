942e8837ebebfa0c27cd259ec525e6915198e1ac
Evan Richardson <richare1@my.erau.edu>
2013-11-01 15:34:21 -0400

    Revert "Revert "layout""
    This reverts commit 03a9d1586b49414e8d77fc0ad3f5772453d9a2a4.

---
+++ FlightPath/src/com/example/flightpath/WpListFragment.java
@@ -0,0 +1,24 @@
+package com.example.flightpath;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WpListFragment extends Fragment{
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		
+
+		return inflater.inflate(R.layout.wp_list, container, false);
+	}
+
+}
