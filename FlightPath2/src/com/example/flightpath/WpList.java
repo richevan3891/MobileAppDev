2dd949aaa78d1925ae2a7b2d2f772ec1ee540637
Evan Richardson <richare1@my.erau.edu>
2013-11-01 16:18:15 -0400

    Flight Path Update

---
+++ FlightPath2/src/com/example/flightpath/WpList.java
@@ -0,0 +1,20 @@
+package com.example.flightpath;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WpList extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+/*
+ * <fragment 
+          android:id="@+id/map"
+   		  android:layout_width="0dp"
+          android:layout_height="match_parent"
+   		  android:layout_weight="70"
+          android:name="com.google.android.gms.maps.MapFragment"/>
+ */
+}
