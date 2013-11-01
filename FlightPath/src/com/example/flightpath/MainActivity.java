8ab7ba5aab752964b5c14c788b24e1ab5ea895e4
latilm <latilm@my.erau.edu>
2013-11-01 15:21:02 -0400

    layout

---
+++ FlightPath/src/com/example/flightpath/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.flightpath;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+
+}
